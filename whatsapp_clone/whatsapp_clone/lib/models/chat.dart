e0f9f864638101ccbf66d368f879512e81d68b1f
EngAmiir <aamin.aamin2010@gmail.com>
2023-12-10 22:15:59 +0300

    last update

--- whatsapp_clone/whatsapp_clone/lib/models/chat.dart
+++ whatsapp_clone/whatsapp_clone/lib/models/chat.dart
@@ -12,38 +12,38 @@ class Chat {
 
 List<Chat> chats = [
   Chat(
-      message: "Sualaha iso dir",
-      name: "Amirkey Osman ",
-      avatar: "https://i.ibb.co/D87bXzr/amirs.jpg",
+      message: "hey setehe",
+      name: "Sunari ",
+      avatar: "https://i.ibb.co/D87bXzr/Amino.jpg",
       time: "11:24 PM"),
   Chat(
-      message: "Oky?",
-      name: "My boy",
-      avatar: "https://i.ibb.co/2vLQsFF/My-Boy.jpg",
+      message: "hellow?",
+      name: "Miski",
+      avatar: "https://i.ibb.co/2vLQsFF/Miski.jpg",
       time: "10:24 PM"),
   Chat(
       message: "Muhimada maxa wye",
-      name: "Lil One",
-      avatar: "https://i.ibb.co/j39H0YR/lilOne.jpg",
+      name: "Peztie",
+      avatar: "https://i.ibb.co/j39H0YR/Peztie.jpg",
       time: "11:30 PM"),
   Chat(
       message: "Thank you.",
-      name: "Zeem Buddy",
-      avatar: "https://i.ibb.co/QpywV7G/Zeem.jpg",
+      name: "Amirkey",
+      avatar: "https://i.ibb.co/QpywV7G/Sec.jpg",
       time: "12:24 AM"),
   Chat(
-      message: "Hey, Maria good night?",
-      name: "Amirah Sapirin",
-      avatar: "https://i.ibb.co/C1VyKHC/sapirin.jpg",
+      message: "Hey, Senorita good night?",
+      name: "Sinorita",
+      avatar: "https://i.ibb.co/C1VyKHC/Sinorita.jpg",
       time: "8:34 AM"),
   Chat(
       message: "ciyow maa imaneh",
-      name: "Bitsy",
-      avatar: "https://i.ibb.co/rtFqDRy/Bitsy.jpg",
+      name: "Sumayo",
+      avatar: "https://i.ibb.co/rtFqDRy/Sumayo.jpg",
       time: "1:56 AM"),
   Chat(
       message: "Haa Sxb",
-      name: "JEY",
-      avatar: "https://i.ibb.co/bFGs189/sec.jpg",
+      name: "Sidra",
+      avatar: "https://i.ibb.co/bFGs189/Sidra.jpg",
       time: "8:05 AM"),
 ];
